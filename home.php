eabb9b90c5e8efa328488d7acb7ab2c261e4d205
Alfonso Crescencio <act280493@gmail.com>
2022-06-21 18:01:04 -0500

    Primera version para git

--- home.php
+++
@@ -1,7 +0,0 @@
-
-    <?php include_once './layouts/header.php'; ?>
-    <figure class="banner__index">
-        <img src="./libs/images/LOGO_NUEVO_CAPREPA_TRANSPARENTE.png" alt="">
-    </figure>
-    <?php include_once './layouts/footer.php'; ?>
-
